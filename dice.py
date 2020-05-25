5b5b7de599dfb2712c07259318de940b85e2e3c6
amisaka <amisaka@gmail.com>
2020-05-25 10:43:10 -0400

    Added comments

--- dice.py
+++ dice.py
@@ -1,31 +1,34 @@
 #!/usr/bin/python3
 
 import serial
-import webbrowser
-ser = serial.Serial("/dev/ttyACM0", 9600)
-ser.flushInput()
+import webbrowser   # version 1
+ser = serial.Serial("/dev/ttyACM0", 9600)     # version 1 
+ser.flushInput()    # version 1
 
 while True:
 	try:
+		# The following two lines are used for
+		# converting a binary text to utf-8
+		# 
 		lface = ser.readline().decode('utf-8').split()
 		face = ''.join(lface)
 		if face == '1':
-		   webbrowser.open('face1.jpg')
+		   webbrowser.open('face1.jpg')   # version 1
 		   # print('One')
 		elif face == '2':
-		   webbrowser.open('face2.jpg')
+		   webbrowser.open('face2.jpg')   # version 1
 		   # print('Two')
 		elif face == '3':
-		   webbrowser.open('face3.jpg')
+		   webbrowser.open('face3.jpg')   # version 1
 		   # print('Three')
 		elif face == '4':
-		   webbrowser.open('face4.jpg')
+		   webbrowser.open('face4.jpg')   # version 1
 		   # print('Four')
 		elif face == '5':
-		   webbrowser.open('face5.jpg')
+		   webbrowser.open('face5.jpg')   # version 1
 		   # print('Five')
 		else:
-		   webbrowser.open('face6.jpg')
+		   webbrowser.open('face6.jpg')   # version 1
 		   # print('Six')
 	except:
 		print("Keyboard Interrupt")
